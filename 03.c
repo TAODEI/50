8503008fc3e1e1042d55d4acf65e18b699fc8cb1
TAODEI <864978550@qq.com>
2020-10-18 17:28:12 +0800

    03

--- 03.c
+++ 03.c
@@ -1,4 +1,20 @@
 #include<stdio.h>
 void main()
 {
-	printf("*****\n ***\n  *\n ***\n*****\n");}
+	int a,i,j;
+	printf("请输入奇数的层数：");
+	scanf("%d",&a);
+	for(i=1;i<=a/2;i++){
+		for(j=1;j<i;j++)
+			putchar(' ');
+		for(j=1;j<a+3-2*i;j++)
+			putchar('*');
+	putchar('\n');
+	}
+	for(i=1;i<=a/2+1;i++){
+		for(j=1;j<=(a+1)/2-i;j++)
+                         putchar(' ');
+                for(j=1;j<=2*i-1;j++)
+                         putchar('*');
+        putchar('\n');}	
+	}
