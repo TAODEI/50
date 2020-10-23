0e1eb631ec9e0ea6c721f0369351fc219cb43a81
TAODEI <864978550@qq.com>
2020-10-23 16:36:27 +0800

    17

---
+++ 17.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+void main()
+{
+	int i,m,n,j,k,s[10000];
+	scanf("%d%d",&m,&n);
+	s[0]=2;
+	for(i=3,k=1;i<90000;i=i+2)
+		for(j=0;j<=k;j++){
+			while(j==k){
+				s[k]=i;
+				k++;break;
+			}
+			if(i%s[j]==0)
+				break;
+		}
+	/*
+		for(k=3;k<=j;k=k+2){
+			while(j==k){
+			s[i]=j;
+			i++;break;
+			}
+			if(j%k==0)break;
+		}*/
+	for(i=m-1;i<n;i++){
+		printf("%d",s[i]);
+		while((i-m+2)%10!=0&&i!=n-1){
+			putchar(' ');break;
+		}
+		while((i-m+2)%10==0&&(i-m+1)!=0){
+			putchar('\n');break;
+		}
+	}	
+}
+
+
+			
