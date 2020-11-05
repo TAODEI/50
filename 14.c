ba02a769870005359be8b2002a686d42d0318cc4
TAODEI <864978550@qq.com>
2020-11-05 19:01:24 +0800

    14

--- 14.c
+++ 14.c
@@ -1,23 +1,28 @@
 #include<stdio.h>
 void main(){
-	int n,i,j[100],k,s;
-       	while(scanf("%d",&n)!=EOF&&n!=0){
-		for(i=0;i<n;i++)
-			scanf("%d",&j[i]);
-		j[n]=1410065407;
-		for(i=0;i<n;i++)
-			for(s=i+1;s<=n;s++){
-				if(j[i]>j[s])
-					break;
-				if(s=n){
-					k=j[i];
-					j[i]=j[0];
-					j[0]=k;
-					i=i+100;
+	int n,i,k,s;
+    while(scanf("%d",&n)!=EOF&&n!=0){
+		int a[100]={0};
+		for(i=0;i<n;i++){
+			scanf("%d",&a[i]);
 		}
+		a[n]=1410065407;
+		for(i=0;i<n;i++){
+			for(s=i+1;s<=n;s++){
+				if(a[i]>a[s]) break;
+				if(s==n){
+					int k;
+					k=a[i];
+					a[i]=a[0];
+					a[0]=k;
+					i+=100;
+				}
 			}
-		for(i=0;i<n;i++)
-			printf("%d ",j[i]);
+		}
+		for(i=0;i<n;i++){
+			if(i!=n-1) printf("%d ",a[i]);
+			else printf("%d",a[i]);
+		}
 		putchar('\n');
-	}}
-
+	}
+}
