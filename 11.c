79170f3007313cb26bb52f1d4ea5baab4eb51ac8
TAODEI <864978550@qq.com>
2020-11-05 19:00:46 +0800

    11

--- 11.c
+++ 11.c
@@ -1,23 +1,31 @@
 #include<stdio.h>
 void main(){
-    int m,n,i,j;
-    while(scanf("%d%d",&n,&m)!=EOF&&(n!=0||m!=0)){
-        signed a[100000];
-        for(i=0;i<n;i++)
-            scanf("%d",&a[i]);
-        for(i=0;i<m;i++){
-            j=i+1;
-            while(j<n){
-                if(a[i]<a[j]){
-                signed temp;
-                temp=a[i];
-                a[i]=a[j];
-                a[j]=temp;}
-                j++;}}
-        if(n>=m){
-            for(i=0;i<m;i++)
-                printf("%d ",a[i]);}
-        else{
-            for(i=0;i<n;i++)
-                           printf("%d ",a[i]);}
-            printf("\n");}}
+	int m,n,i,j;
+	while(scanf("%d%d",&n,&m)!=EOF&&(n!=0||m!=0)){
+		signed a[100000];
+		for(i=0;i<n;i++) scanf("%d",&a[i]);
+		for(i=0;i<m;i++){
+			j=i+1;
+			while(j<n){
+				if(a[i]<a[j]){
+				signed temp;
+				temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;}
+				j++;
+			}
+		}
+		if(n>=m){
+			for(i=0;i<m;i++){
+				if(i!=m-1) printf("%d ",a[i]);
+				else printf("%d",a[i]);
+			}
+		}else{
+			for(i=0;i<n;i++){
+				if(i!=n-1) printf("%d ",a[i]);
+				else printf("%d",a[i]);
+			}
+		}
+		putchar('\n');
+	}
+}
