0886121e81784fe374319e44b625f7965c22452e
TAODEI <864978550@qq.com>
2020-11-05 18:59:43 +0800

    06

--- 06.c
+++ 06.c
@@ -1,28 +1,25 @@
 #include<stdio.h>
 #include<math.h>
-void main()
-{
+void main(){
 	int num[100];
 	int n;
-	while(~scanf("%d",&n)&&n>0)
-	{
+	while(~scanf("%d",&n)&&n>0){
 		int i, j, k;
 		for( i=0; i<n ;i++)
 		scanf("%d",&num[i]);
 		for( j=0; j<n ;j++){
-		for( k=j+1 ;k<n ;k++)
-		{
-			if(fabs(num[j])<fabs(num[k]))
-			{
-				int temp;
-				temp=num[j];
-				num[j]=num[k];
-				num[k]=temp;
-			}}}
-		for( i=0; i<n; i++)
-		{
+			for( k=j+1 ;k<n ;k++){
+				if(fabs(num[j])<fabs(num[k])){
+					int temp;
+					temp=num[j];
+					num[j]=num[k];
+					num[k]=temp;
+				}
+			}
+		}
+		for(i=0; i<n; i++){
 			printf("%d",num[i]);
-			putchar(' ');
+			if(i!=n-1) putchar(' ');
 		}
 		printf("\n");
 	}	
