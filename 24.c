e6d6a002c5cef0efb3c33573a0e534ba9ffa04c4
tao <864978550@qq.com>
2020-11-06 19:36:01 +0800

    24

--- 24.c
+++ 24.c
@@ -3,7 +3,7 @@
 void main(){
 	int i,sum=0;
 	char a[][5]={"ling","yi","er","san","si","wu","liu","qi","ba","jiu"};
-	char n[100]="0";
+	char n[101];
 	scanf("%s",n);
 	for(i=0;i<strlen(n);i++) sum+=n[i]-'0';
 	int b,c,d;
@@ -11,6 +11,6 @@ void main(){
 	c=sum/10%10;
 	d=sum/100;
 	if(d!=0) printf("%s ",a[d]);
-	if(c!=0) printf("%s ",a[c]);
-	printf("%s",a[b]);
+	if(d!=0||c!=0) printf("%s ",a[c]);
+	printf("%s\n",a[b]);
 }
