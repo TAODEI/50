8ad1ce880cea8ebeca403b0a2d85e8c081c04279
tao <864978550@qq.com>
2020-11-12 16:03:47 +0800

    11

--- 11.c
+++ 11.c
@@ -2,17 +2,16 @@
 void main(){
 	int m,n,i,j;
 	while(scanf("%d%d",&n,&m)!=EOF&&(n!=0||m!=0)){
-		signed a[100000];
+		int a[100000];
 		for(i=0;i<n;i++) scanf("%d",&a[i]);
 		for(i=0;i<m;i++){
-			j=i+1;
-			while(j<n){
+			for(j=i+1;j<n;j++){
 				if(a[i]<a[j]){
-				signed temp;
+				int temp;
 				temp=a[i];
 				a[i]=a[j];
-				a[j]=temp;}
-				j++;
+				a[j]=temp;
+				}
 			}
 		}
 		if(n>=m){
